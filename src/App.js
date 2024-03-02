9d1e88b8182dcc62fe4efa7c37844c2df379e791
Vincent <vincent.fodor@online.de>
2024-03-03 00:44:13 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { ShoppingList } from './components/ShoppingList';
 
 function App() {
+  const [username, setUsername] = useState(localStorage.getItem("username"))
+
+  useEffect(() => {
+    if (!username) {
+      promptUsername();
+    }
+  }, []);
+
+  const promptUsername = () => {
+    const newUsername = prompt("sag mir dein name", "Phil");
+
+    if(!newUsername) {
+      return promptUsername();
+    }
+
+    setUsername(newUsername);
+
+    localStorage.setItem("username", newUsername);
+  }
+
+  const renderTitle = username?.endsWith("s") ? `📔 ${username}' Liste` : `📔 ${username}s Liste`
+
   return (
     <div className=''>
-      <Header title="📔 Phils Liste" />
+      <Header title={renderTitle} />
       <ShoppingList storageName="main" itemStorageName="main-items" />
     </div>
   );
